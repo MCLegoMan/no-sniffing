39b9775939e1f2981fe0b1bf61ddaa8196806c00
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-07-10 23:40:29 +1200

    1.1 - Quilt/Fabric/NeoForge version

--- src/main/java/com/mclegoman/nosniffing/client/ClientMain.java
+++
@@ -1,9 +0,0 @@
-package com.mclegoman.nosniffing.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ClientMain implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
